759164b67056c99b196f31875b0344f5d0b2a076
M.Zidan <mostafazidan3991@gmail.com>
2022-12-24 17:34:58 +0200

    Initial Commit (Project Creation).

---
+++ ITSquare/ITSquare/ITSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITSquareApp.swift
+//  ITSquare
+//
+//  Created by M.M.Z on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ITSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
